198a497c483132d0d0764bd1c2910edd5750ffbe
mani333007 <mani333007@gmail.com>
2021-01-23 23:58:05 +0530

    Added currently selected tiny image to blue border

--- src/components/ProductsUi.jsx
+++ src/components/ProductsUi.jsx
@@ -3,22 +3,27 @@ import './css/productUi.css';
 
 const ProductsUi = () => {
     const [pics, setPics] = useState([
-        { id: 1, src: 'https://images-na.ssl-images-amazon.com/images/I/31DNFLX-5FL.jpg', alt:'a' },
-        { id: 2, src: 'https://images-na.ssl-images-amazon.com/images/I/21armwyupbL.jpg', alt:'b' },
-        { id: 3, src: 'https://images-na.ssl-images-amazon.com/images/I/21e0zJukTBL.jpg', alt:'c' },
-        { id: 4, src: 'https://images-na.ssl-images-amazon.com/images/I/21BrHvAylSL.jpg', alt:'d' },
-        { id: 5, src: 'https://images-na.ssl-images-amazon.com/images/I/11M6F9eAc4L.jpg', alt:'e' },
-        { id: 6, src: 'https://images-na.ssl-images-amazon.com/images/I/11XZ22EG9QL.jpg', alt:'f' },
-        { id: 7, src: 'https://images-na.ssl-images-amazon.com/images/I/41w62fQzA%2BL.jpg', alt:'g' }
+        { id: 1, src: 'https://rukminim1.flixcart.com/image/416/416/kgiaykw0/mobile/y/n/y/apple-iphone-11-mhda3hn-a-original-imafwqepdb3fxtug.jpeg?q=70', alt:'a', selected: true },
+        { id: 2, src: 'https://rukminim1.flixcart.com/image/416/416/kgiaykw0/mobile/y/n/y/apple-iphone-11-mhda3hn-a-original-imafwqepmmrxjzzd.jpeg?q=70', alt:'b', selected: false },
+        { id: 3, src: 'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/x/j/s/iphone-11-128-d-mwm02hn-a-apple-0-original-imafkg242ugz8hwc.jpeg?q=70', alt:'c', selected: false },
+        { id: 4, src: 'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/v/w/j/iphone-11-128-a-mwm02hn-a-apple-0-original-imafkg24bhgxu9ux.jpeg?q=70', alt:'d', selected: false },
+        { id: 5, src: 'https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/d/u/h/iphone-11-128-u-mwm02hn-a-apple-0-original-imafkg24r2ragyqf.jpeg?q=70', alt:'e', selected: false },
     ]);
 
     const [mainPic, setMainPic] = useState(pics[0]);
+
+    const handleSelectMainPick = (picObj) => {
+        setMainPic(picObj);
+        // changed selected property to true
+        const updatedPics = pics.map(pic => pic.id === picObj.id ? { ...pic, selected: true } : { ...pic, selected: false });
+        setPics(updatedPics);
+    }
 
     return ( 
         <div className="product-pics">
             <div className="select-pics">
                 {pics.map((pic) => (
-                    <img key={pic.id} src={pic.src} alt={pic.alt} className='tiny-pic' onClick={() => setMainPic(pic)} />
+                    <img key={pic.id} src={pic.src} alt={pic.alt} className='tiny-pic' style={{ border: pic.selected && '2px solid #3366ff' }} onMouseOver={() => handleSelectMainPick(pic)} />
                 ))}
             </div>
             <div className="main-pic">
